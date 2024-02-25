4a2016b9605c5ea85bbb01d94b9331229fda882b
tatsupiko <identity2022@outlook.jp>
2024-02-25 16:27:07 +0900

    [update] BulletBaseに「各種イベントの追加」と「プロパティの追加・編集」をした。

--- Assets/Common/Scripts/Bullet/BulletBase.cs
+++ Assets/Common/Scripts/Bullet/BulletBase.cs
@@ -1,12 +1,41 @@
 using Cysharp.Threading.Tasks;
 using System;
+using System.Net;
 using UnityEngine;
 
 // 日本語対応
-public class BulletBase : MonoBehaviour, IBulletData
+public abstract class BulletBase : MonoBehaviour, IBulletData
 {
-    public float Speed { get => _initSpeed; set => _initSpeed = value; }
-    public int Damage { get => _initDamage; set => _initDamage = value; }
+    [SerializeField] private float _initSpeed = 1.0f;
+    [SerializeField] private int _initDamage = 0;
+    [SerializeField] private float _coolTime = 1.0f;
+    [SerializeField] private Rigidbody2D _rigidbody2D = null;
+
+    /// <summary>自身を撃ったオブジェクトのレイヤー</summary>
+    private LayerMask _gunnerLayer = 0;
+
+    protected Rigidbody2D Rigidbody2D => _rigidbody2D;
+
+    protected event Action OnAwakeEvent = null;
+    protected event Action OnEnableEvent = null;
+    protected event Action OnStartEvent = null;
+    protected event Action OnFixedUpdateEvent = null;
+    protected event Action OnUpdateEvent = null;
+    protected event Action OnLateUpdateEvent = null;
+    protected event Action OnDisableEvent = null;
+    protected event Action OnDestroyEvent = null;
+    protected event Action<GameObject> OnTriggerEnterEvent = null;
+    protected event Action<GameObject> OnTriggerStayEvent = null;
+    protected event Action<GameObject> OnTriggerExitEvent = null;
+
+    protected abstract void BaseEventRegister();
+    protected abstract void BaseEventUnregister();
+
+    public float Speed { get => _rigidbody2D.velocity.magnitude;
+                         set => _rigidbody2D.velocity = _rigidbody2D.velocity.normalized * Mathf.Max(value, 0.0f); }
+    public Vector2 Direction { get => _rigidbody2D.velocity.normalized; set => _rigidbody2D.velocity = value * Speed; }
+    public int Damage { get => _initDamage; set => _initDamage = Mathf.Max(value, 0); }
+    public float CoolTime { get => _coolTime; set => _coolTime = Mathf.Max(value, 0.0f); }
     /// <summary>Colliderに接触したかどうか</summary>
     public bool IsTriggerEnter { get; private set; } = false;
     /// <summary>Colliderに接触しているかどうか</summary>
@@ -14,27 +43,90 @@ public class BulletBase : MonoBehaviour, IBulletData
     /// <summary>接触していたColliderから出たかどうか</summary>
     public bool IsTriggerExit { get; private set; } = false;
 
-    protected Rigidbody2D Rigidbody2D => _rigidbody2D;
-    /// <summary>Colliderに接触したときに実行するイベント</summary>
-    protected event Action<GameObject> OnTriggerEnterEvent;
-    /// <summary>Colliderに接触しているあいだ実行するイベント</summary>
-    protected event Action<GameObject> OnTriggerStayEvent;
-    /// <summary>接触していたColliderから出たときに実行するイベント</summary>
-    protected event Action<GameObject> OnTriggerExitEvent;
+    public virtual void Init(LayerMask layer)
+    {
+        _gunnerLayer = layer;
+    }
 
-    [SerializeField] private float _initSpeed = 1.0f;
-    [SerializeField] private int _initDamage = 0;
-    [SerializeField] private Rigidbody2D _rigidbody2D = null;
-
-    /// <summary>自身を撃ったオブジェクトのレイヤー</summary>
-    private LayerMask _gunnerLayer = 0;
-    private event Action<GameObject> _onTriggerEnter = null;
-    private event Action<GameObject> _onTriggerStay = null;
-    private event Action<GameObject> _onTriggerExit = null;
-
+    #region General LifeCycle
+    private void Awake()
+    {
+        OnAwakeEvent?.Invoke();
+    }
+    private void OnEnable()
+    {
+        OnEnableEvent?.Invoke();
+    }
     private void Start()
     {
-        #region Initialize Regidbody
+        BaseInit();
+        OnStartEvent?.Invoke();
+    }
+    private void FixedUpdate()
+    {
+        OnFixedUpdateEvent?.Invoke();
+    }
+    private void Update()
+    {
+        OnUpdateEvent?.Invoke();
+    }
+    private void LateUpdate()
+    {
+        OnLateUpdateEvent?.Invoke();
+    }
+    private void OnDisable()
+    {
+        OnDisableEvent?.Invoke();
+    }
+    private void OnDestroy()
+    {
+        OnDestroyEvent?.Invoke();
+    }
+    #endregion
+
+    #region OnTriggerEvent
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == _gunnerLayer) return;
+
+        OnTriggerEnterEvent?.Invoke(collision.gameObject);
+        DetectionTriggerEnter();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == _gunnerLayer) return;
+
+        OnTriggerStayEvent?.Invoke(collision.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == _gunnerLayer) return;
+
+        OnTriggerExitEvent?.Invoke(collision.gameObject);
+        DetectionTriggerExit();
+    }
+    /// <summary>Colliderの接触を検知したときに呼び出す</summary>
+    private async void DetectionTriggerEnter()
+    {
+        IsTriggerStay = true;
+        IsTriggerEnter = true;
+        await UniTask.DelayFrame(1);
+        IsTriggerEnter = false;
+    }
+    /// <summary>接触していたColliderから出たときに呼び出す</summary>
+    private async void DetectionTriggerExit()
+    {
+        IsTriggerStay = false;
+        IsTriggerExit = true;
+        await UniTask.DelayFrame(1);
+        IsTriggerExit = false;
+    }
+    #endregion
+
+    private void BaseInit()
+    {
         if (!_rigidbody2D)
         {
             _rigidbody2D = TryGetComponent(out Rigidbody2D rb2d) switch
@@ -44,7 +136,6 @@ public class BulletBase : MonoBehaviour, IBulletData
             };
         }
         _rigidbody2D.bodyType = RigidbodyType2D.Kinematic;
-        #endregion
 
         switch (TryGetComponent(out Collider2D collider))
         {
@@ -53,49 +144,5 @@ public class BulletBase : MonoBehaviour, IBulletData
             case false:
                 Debug.Log("Collider2D is not found"); break;
         }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.layer == _gunnerLayer) return;
-
-        _onTriggerEnter?.Invoke(collision.gameObject);
-        DetectionTriggerEnter();
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.gameObject.layer == _gunnerLayer) return;
-
-        _onTriggerStay?.Invoke(collision.gameObject);
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.layer == _gunnerLayer) return;
-
-        _onTriggerExit?.Invoke(collision.gameObject);
-        DetectionTriggerExit();
-    }
-
-    private async void DetectionTriggerEnter()
-    {
-        IsTriggerStay = true;
-        IsTriggerEnter = true;
-        await UniTask.DelayFrame(1);
-        IsTriggerEnter = false;
-    }
-
-    private async void DetectionTriggerExit()
-    {
-        IsTriggerStay = false;
-        IsTriggerExit = true;
-        await UniTask.DelayFrame(1);
-        IsTriggerExit = false;
-    }
-
-    public virtual void Init(LayerMask layer)
-    {
-        _gunnerLayer = layer;
     }
 }
