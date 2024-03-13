234b6a956ab3b72b4704e54ee7494376cf97c181
AkiraWatanabe23 <a0323w@gmail.com>
2024-03-13 14:29:20 +0900

    [add] 敵の生成後の動き

---
+++ Assets/Script/Enemy/System/SpawnSystem/Assault/FigureEight.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+/// <summary> 8の字 </summary>
+[Serializable]
+public class FigureEight
+{
+
+}
---
+++ Assets/Script/Enemy/System/SpawnSystem/Assault/RightAngle.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+/// <summary> 直角に曲がる </summary>
+[Serializable]
+public class RightAngle
+{
+
+}
