abb3825de8ce1037724f42629df3dc820a15b828
AkiraWatanabe23 <a0323w@gmail.com>
2024-03-21 23:55:11 +0900

    [fix] Player仕様変更

---
+++ Assets/Script/Player/Logic/PlayerAttack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class PlayerAttack : PlayerSystemBase
+{
+    [SerializeField]
+    private int _attackValue = 1;
+    [SerializeField]
+    private float _attackInterval = 1f;
+    [SerializeField]
+    private BulletController _bulllet = default;
+
+    public int AttackValue => _attackValue;
+
+    protected bool IsGetShootInput => Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0);
+
+    public override void OnUpdate()
+    {
+        if (IsGetShootInput) { Attack(); }
+    }
+
+    private void Attack()
+    {
+        Debug.Log("attack");
+    }
+}
---
+++ Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour, IDamageable
+{
+    [SerializeField]
+    private PlayerMovement _movement = new();
+    [SerializeField]
+    private PlayerAttack _attack = new();
+    [SerializeField]
+    private PlayerHealth _health = new();
+
+    private void Start()
+    {
+        _movement.Initialize(gameObject);
+        _attack.Initialize(gameObject);
+        _health.Initialize(gameObject);
+    }
+
+    private void Update()
+    {
+        _movement.OnUpdate();
+        _attack.OnUpdate();
+    }
+
+    public void ReceiveDamage(int value) { _health.ReceiveDamage(value); }
+}
