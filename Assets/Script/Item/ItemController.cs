f2ed79058425ce5eb4b263fba14dccb7e0c34bf5
AkiraWatanabe23 <a0323w@gmail.com>
2024-03-28 22:12:29 +0900

    [add] Enemy全体破壊

--- Assets/Script/Item/ItemController.cs
+++ Assets/Script/Item/ItemController.cs
@@ -7,4 +7,10 @@ public class ItemController : MonoBehaviour
     private IGameItem _itemSystem = default;
 
     public IGameItem ItemSystem => _itemSystem;
+
+    /// <summary> 画面外に出たら削除 </summary>
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
 }
