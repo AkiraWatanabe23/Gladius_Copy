4436b112d369d7c87de03c75220ea2dbedf008c0
AkiraWatanabe23 <a0323w@gmail.com>
2024-04-01 23:11:05 +0900

    [update] AssaultEnemy移動パターン2 - 仮実装

--- Assets/Script/Enemy/System/SpawnSystem/Assault/RightAngle.cs
+++ Assets/Script/Enemy/System/SpawnSystem/Assault/RightAngle.cs
@@ -1,14 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
+using UnityEngine;
 
 /// <summary> 直角に曲がる </summary>
 [Serializable]
 public class RightAngle : IEnemyGeneration
 {
-    public List<EnemyController> Enemies { get; set; }
+    protected EnemyManager EnemyManager { get; private set; }
+
+    public RightAngle(EnemyManager enemyManager)
+    {
+        EnemyManager = enemyManager;
+    }
 
     public void Movement(EnemyController enemy)
     {
+        if (enemy.EnemySystem is not Assault) { return; }
 
+        var assault = (Assault)enemy.EnemySystem;
+        if (!assault.IsFinishMoveUp)
+        {
+            assault.Rb2d.velocity = Vector2.up * enemy.MoveSpeed;
+            assault.IsFinishMoveUp = MovementSearch(assault);
+        }
+        else
+        {
+            assault.Rb2d.velocity = Vector2.left * enemy.MoveSpeed;
+        }
     }
+
+    private bool MovementSearch(Assault assault)
+        => Mathf.Abs(assault.Transform.position.y - EnemyManager.PlayerTransform.position.y) < 0.1f;
 }
