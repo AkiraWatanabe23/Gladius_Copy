5c3c369df177dc50e4efe72a409190918b3db7d1
AkiraWatanabe23 <a0323w@gmail.com>
2024-04-01 23:50:23 +0900

    [add] ShotEnemy移動パターン

---
+++ Assets/Script/Enemy/System/SpawnSystem/Shot/CrawlGround.cs
@@ -0,0 +1,9 @@
+﻿public class CrawlGround : IEnemyGeneration
+{
+    public void Movement(EnemyController enemy)
+    {
+        if (enemy.EnemySystem is not Shot) { return; }
+
+        var shot = (Shot)enemy.EnemySystem;
+    }
+}
