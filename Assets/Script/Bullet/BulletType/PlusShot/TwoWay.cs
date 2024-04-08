1d956300c22ecb92d2cb9e47d06363af6284731b
AkiraWatanabe23 <a0323w@gmail.com>
2024-04-08 23:12:36 +0900

    [add] プラスショット - 2way

---
+++ Assets/Script/Bullet/BulletType/PlusShot/TwoWay.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class TwoWay : IBulletData
+{
+    public GameObject BulletObj { get; set; }
+    public Transform Transform { get; set; }
+    public float Speed { get; set; }
+    public int AttackValue { get; set; }
+    public LayerMask GunnerLayer { get; set; }
+    public Vector2 MoveDirection { get; set; }
+    public Rigidbody2D Rb2d { get; set; }
+
+    public void Movement()
+    {
+        Rb2d.velocity = MoveDirection * Speed;
+    }
+
+    public void Hit(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out IDamageable damageTarget))
+        {
+            damageTarget.ReceiveDamage(AttackValue);
+            GameManager.Instance.ObjectPool.RemoveObject(BulletObj);
+        }
+        else if (collision.gameObject.TryGetComponent(out Ground _))
+        {
+            GameManager.Instance.ObjectPool.RemoveObject(BulletObj);
+        }
+    }
+}
+
+public enum Diagonal
+{
+    Up,
+    Down
+}
